a890a04b78bf99a7e0065e6779be2df206c06caa
filipw <wojcieszyn@gmail.com>
2021-02-06 18:18:25 +0100

    added host to before

---
+++ before/Host/Driver/Driver.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Lib;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace CSharpHost
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var qsim = new QuantumSimulator();
+
+            var randomBits = await RandomNumberGenerator.Run(qsim);
+            var bitString = string.Join("", randomBits.Select(x => x ? 1 : 0));
+
+            Console.WriteLine($"Generated random bit string: {bitString}");
+            Console.WriteLine($"Generated random uint32: {Convert.ToUInt32(bitString, 2)}");
+        }
+    }
+}
